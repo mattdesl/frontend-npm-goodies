3e660c99823404067026cd4e12608c7802caa9ef
mattdesl <dave.des@gmail.com>
2014-11-10 00:20:04 -0500

    removing bunny from svg

--- webgl/svg.js
+++ webgl/svg.js
@@ -1,5 +1,4 @@
 var background = require('gl-checker-background')
-var bunny = require('bunny')
 var rgb = require('./luma-to-rgb')
 
 //get the triangulated svg mesh
@@ -8,6 +7,6 @@ var mesh = require('./svg-mesh')()
 var scale = 10
 require('./orbit-viewer')(mesh, {
     scale: [scale, -scale, scale], //scale the model matrix
-    tint: [0.15,0.15,0.2,0.90],  //mesh color
-    background: background     //pass the factory function for our background
+    tint: [0.15,0.15,0.2,0.90],    //mesh color
+    background: background         //pass the factory function for our background
 })
